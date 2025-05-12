bc2c6aff3dd189573aaa721e9e6adfb779ab21b2
Melvin <melvin11278@gmail.com>
2025-05-12 16:16:55 +0700

    updating to toggle and its function to deleting task

--- to-do-list/todo.py
+++ to-do-list/todo.py
@@ -7,35 +7,38 @@ root.geometry("600x450")
 label = tk.Label(root,text="To-Do List" ,font=("Calibri", 18))
 label.pack(pady=10)
 
-# list of task shown on window
-task = tk.Listbox(root, width=50, height=20)
-task.pack(pady=10)
-
 task_entry = tk.Entry(root, width=40)
 task_entry.pack(pady=10)
 
+task_frame = tk.Frame(root)
+task_frame.pack(pady=10)
+# storing in checkboxes
+checkboxes = []
 
 # to add a task
 def add_new_task():
     task_text = task_entry.get()
     if(task_text):
-        task.insert(tk.END, task_text)
+        var = tk.IntVar() # save a status (0 and 1) for each checckbox
+        checkbox = tk.Checkbutton(task_frame, text=task_text, variable=var, onvalue=1, offvalue=0)
+        checkbox.pack(anchor="w")
+        checkboxes.append((checkbox, var))
         task_entry.delete(0, tk.END)
 
-# to delete a task
-def delete_task():
-    try:
-        particular_idx = task.curselection()[0];
-        task.delete(particular_idx)
-    except IndexError:
-        pass
+
+# delete a task
+def delete_completed_task():
+    for checkbox, var in checkboxes[:]:
+        if var.get() == 1: # if checkbox selected
+            checkbox.pack_forget() # hide a checkbox
+            checkboxes.remove((checkbox, var)) # remove every toggled checkbox from the list.
 
 
 add_button = tk.Button(root, text="Add New Task", command=add_new_task)
 add_button.pack(pady=10)
 
-delete_button = tk.Button(root, text="Delete Task", command=delete_task)
-delete_button.pack(pady=10)
+delete_button = tk.Button(root, text="Delete Task", command=delete_completed_task)
+delete_button.pack(pady=10) 
 
 
 root.mainloop()
