fe5d375819cdae59bce093797810a155b9e42568
Melvin <melvin11278@gmail.com>
2025-05-12 16:28:18 +0700

    add a style and color for the  button

--- to-do-list/todo.py
+++ to-do-list/todo.py
@@ -3,11 +3,12 @@ import tkinter as tk
 root = tk.Tk()
 root.title("To-do list app")
 root.geometry("600x450")
+root.config(bg="black")
 
-label = tk.Label(root,text="To-Do List" ,font=("Calibri", 18))
+label = tk.Label(root,text="To-Do List" ,font=("Calibri", 25, "bold"), fg="white", bg="black")
 label.pack(pady=10)
 
-task_entry = tk.Entry(root, width=40)
+task_entry = tk.Entry(root, width=40, relief="solid")
 task_entry.pack(pady=10)
 
 task_frame = tk.Frame(root)
@@ -34,10 +35,10 @@ def delete_completed_task():
             checkboxes.remove((checkbox, var)) # remove every toggled checkbox from the list.
 
 
-add_button = tk.Button(root, text="Add New Task", command=add_new_task)
+add_button = tk.Button(root, text="Add New Task", command=add_new_task, bg="#4CAF50", relief="raised", font=("Calibri", 12), fg="white")
 add_button.pack(pady=10)
 
-delete_button = tk.Button(root, text="Delete Task", command=delete_completed_task)
+delete_button = tk.Button(root, text="Delete Task", command=delete_completed_task, bg="#F44336", font=("Calibri", 12), bd=2, fg="white")
 delete_button.pack(pady=10) 
 
 
